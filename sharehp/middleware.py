56000dda93fdf08e2d0b39c65e58f84d151b4668
diaocow <diaocow@163.com>
2014-02-18 19:23:34 +0800

    update

--- sharehp/middleware.py
+++ sharehp/middleware.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 import cache
-import json
 
-# build xmanuser
+# build request xmanuser
 class LoginSessionMiddleWare(object):
     def process_request(self, request):
         request.xmanuser = {'login': False}
         session_id = request.COOKIES.get('id')
+
         if session_id:
             session_data = cache.get_login_session(session_id)
             if session_data:
                 request.xmanuser['login'] = True
-                request.xmanuser.update(json.loads(session_data))
+                request.xmanuser.update(session_data)
 			
 		
 		
