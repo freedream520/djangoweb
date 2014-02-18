1650d3ea0553362b4dfe14a970de6675ce910e61
diaocow <diaocow@163.com>
2014-02-18 19:22:53 +0800

    update

--- sharehp/qiniu_helper.py
+++ sharehp/qiniu_helper.py
@@ -1,10 +1,29 @@
 import qiniu.rs
 import qiniu.conf
 import qiniu.io
-import os
+from utils import config
+from exception import ConfigArgumentError
+from exception import QiniuUploadFileError
+
+_ACCESS_KEY = config.get_config("QINIU_ACCESS_KEY")
+_SECRET_KEY = config.get_config("QINIU_SECRET_KEY")
+_STATIC_NAME = config.get_config("QINIU_STATIC_NAME")
+
+if not _ACCESS_KEY or not _SECRET_KEY or not _STATIC_NAME:
+    raise ConfigArgumentError("qiniu config is invalid!")
+
+qiniu.conf.ACCESS_KEY = _ACCESS_KEY
+qiniu.conf.SECRET_KEY = _SECRET_KEY
+
+policy = qiniu.rs.PutPolicy(_STATIC_NAME)
+uptoken = policy.token()
 
 
-#ret, err = qiniu.io.put_file(uptoken, 'thumbnail/' + file, dir + file)
-
-def upload(url, file_path):
-    pass
+def upload(filepath, key):
+    try:
+        ret, err = qiniu.io.put_file(uptoken, key, filepath)
+    except Exception, err: # catch all exception TODO log
+        raise QiniuUploadFileError(err)
+    else:
+        if err is not None:
+            raise QiniuUploadFileError(err)
