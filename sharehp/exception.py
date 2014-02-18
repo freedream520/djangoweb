3ed4ffebe7ab674cc41d2ec1bd411cadd19503da
diaocow <diaocow@163.com>
2014-02-18 19:24:24 +0800

    add

---
+++ sharehp/exception.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# TODO
+class IllegalArgumentError(ValueError):
+    pass
+
+
+class ConfigArgumentError(ValueError):
+    pass
+
+
+class QiniuUploadFileError(IOError):
+    pass
