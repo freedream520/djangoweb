42ee747af438e7330727130ea059c384ebd32369
diaocow <diaocow@163.com>
2014-02-22 18:20:00 +0800

    reverse whitespace

--- sharehp/templatetags/poll_extras.py
+++ sharehp/templatetags/poll_extras.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
 from django import template
 from  ..utils import config
+from django.utils.safestring import mark_safe
 
+
+def white_space(str):
+    if str:
+        str = mark_safe(str.replace(' ', '&nbsp;'))
+    return str
 
 def static_url(resource):
     return config.get_config('QINIU_STATIC_CDN') + resource
 
+def tmp_server_url(resource):
+    return config.get_config('SHAREHP_SERVER_TMP_DIR') + resource
+
+
 register = template.Library()
+register.filter('white_space', white_space)
 register.filter('static_url', static_url)
+register.filter('tmp_server_url', tmp_server_url)
