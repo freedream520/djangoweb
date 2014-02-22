fab440157a9c6facb3344cd46608004c957fc064
diaocow <diaocow@163.com>
2014-02-22 18:12:56 +0800

    add comment

--- sharehp/models.py
+++ sharehp/models.py
@@ -8,7 +8,7 @@ class User(models.Model):
     email = models.CharField(max_length=128)
     nickname = models.CharField(max_length=32)
     password = models.CharField(max_length=32)
-    avatar = models.CharField(max_length=1024) # jsonstr
+    avatar = models.CharField(max_length=1024)  # jsonstr
     status = models.CharField(max_length=32)
 
 
@@ -16,10 +16,12 @@ class Resource(models.Model):
     gmt_create = models.DateTimeField()
     gmt_modify = models.DateTimeField()
     user_id = models.IntegerField()
+    # 资源标题
     title = models.CharField(max_length=10240)
     type = models.CharField(max_length=32)
-    nums = models.IntegerField() # Deprecated
+    # 资源缩略图 {'url': ..., 'size': ...}
     thumbnail = models.CharField(max_length=255)
+    # 资源内容 {'url': ..., 'size': ...}
     content = models.CharField(max_length=10240)
     good = models.IntegerField()
     bad = models.IntegerField()
@@ -32,6 +34,7 @@ class Resource_Comment(models.Model):
     gmt_modify = models.DateTimeField()
     res_id = models.IntegerField()
     user_id = models.IntegerField()
+    # 评论内容 plain text
     content = models.CharField(max_length=10240)
     have_reply = models.CharField(max_length=1)
     status = models.CharField(max_length=32)
@@ -42,6 +45,7 @@ class Group(models.Model):
     gmt_modify = models.DateTimeField()
     group_name = models.CharField(max_length=256)
     group_desc = models.CharField(max_length=512)
+    avatar = models.CharField(max_length=1024)  # jsonstr
     # status = models.CharField(max_length=16)
 
 
@@ -51,7 +55,6 @@ class Group_Topic(models.Model):
     group_id = models.IntegerField()
     user_id = models.IntegerField()
     topic_name = models.CharField(max_length=256)
-    content = models.CharField(max_length=10240)  # not used
     comments = models.IntegerField()
     status = models.CharField(max_length=16)
 
@@ -61,7 +64,14 @@ class Topic_Comment(models.Model):
     gmt_modify = models.DateTimeField()
     user_id = models.IntegerField()
     topic_id = models.IntegerField()
+    # 话题内容
     content = models.CharField(max_length=10240)
+    # 附件
+    # {'attachment': {'type': ...,
+    #                'size': ...,
+    #                'url':...,
+    #                'exist': ...},
+    attachment = models.CharField(max_length=1024)
     status = models.CharField(max_length=16)
 
 
@@ -70,8 +80,16 @@ class Spide_Resource(models.Model):
     gmt_create = models.DateTimeField()
     title = models.CharField(max_length=1024)
     type = models.CharField(max_length=32)
+    # 资源内容
+    # {
+    #   'thumbnail_path': ..., 缩略图路径
+    #   'normal_path': ...,  原始图片路径(video unused)
+    #   'url': ... 视频swf(image unused)
+    # }
     content = models.CharField(max_length=1024)
-    origin =  models.CharField(max_length=1024)
+    # 爬取资源的原始url
+    origin = models.CharField(max_length=1024)
+    # 原始url的md5值（防止重复爬取）
     md5 = models.CharField(max_length=256)
     status = models.CharField(max_length=32)
 
