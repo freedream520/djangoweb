25c6b29d5159afded2ff164f885aaaabfdf43487
diaocow <diaocow@163.com>
2014-02-22 18:18:20 +0800

    ^_^wsgi interface

--- sharehp/utils/image.py
+++ sharehp/utils/image.py
@@ -4,6 +4,7 @@ import Image
 import math
 import urllib
 import qiniu_helper
+import imghdr
 
 # 保存url图片
 def save_img(img_url, target_dir):
@@ -23,10 +24,19 @@ def save_img(img_url, target_dir):
 
 # 压缩图片
 def thumbnail_img(filename, filedir):
+    filepath = filedir + filename
+    # 对于gif类型图片，原图即为缩略图 FIXME
+    if get_image_type(filepath) == 'gif':
+        return {
+            'name': filename,
+            'path': filepath,
+            'size': get_image_size(filepath)
+        }
+
     crop = False
     # 缩略图尺寸
     resize = (210, 420)
-    with open(filedir + filename, 'rb') as f:
+    with open(filepath, 'rb') as f:
         img = Image.open(f)
         ori_width, ori_height = img.size
         _, new_height = img.size
@@ -53,11 +63,11 @@ def thumbnail_img(filename, filedir):
                               mark_img.convert('RGBA'))
 
         # 保存缩略图
-        target_path = filedir + 'thumbnail/' + filename
+        target_path = filedir + filename + '.thumb'
         new_img.save(target_path, "JPEG")
     # end
     return {
-        'name': filename,
+        'name': filename + '.thumb',
         'path': target_path,
         'size': get_image_size(target_path)
     }
@@ -92,13 +102,18 @@ def crop_user_avatar(filedir, filename, box):
             'path': target_path + '.small'},
     }
 
+
 # 获取图片尺寸
 def get_image_size(filepath):
     img = Image.open(filepath)
     return img.size
+
 
 # 上传文件至七牛
 def qiniu_upload(filepath, tgturl):
     qiniu_helper.upload(filepath, tgturl)
 
 
+# 获取图片类型，如果不是图片返回None
+def get_image_type(filepath):
+    return imghdr.what(filepath)
