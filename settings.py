31a3071a608cc4d7c28c8c33a18e442891689eb1
diaocow <diaocow@163.com>
2014-02-23 20:38:43 +0800

    modify log setting

--- settings.py
+++ settings.py
@@ -139,25 +139,17 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-    'console': {
-    'level': 'DEBUG',
-    'class': 'logging.StreamHandler'
-    },
-    'mail_admins': {
-        'level': 'DEBUG',
-        'class': 'django.utils.log.AdminEmailHandler'
-    }
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': '/home/diaocow/error.log',
+        },
     },
     'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
+        'sharehp': {
+            'handlers': ['file'],
             'level': 'ERROR',
             'propagate': True,
-        },
-        'django.db.backends': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    }
+            },
+    },
 }
