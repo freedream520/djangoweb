8e331bdfb9b762a4bcf7089e8750a86efb25edcd
diaocow <diaocow@163.com>
2014-02-24 15:08:52 +0800

    1. bugfix load config.yaml

--- sharehp/utils/config.py
+++ sharehp/utils/config.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import yaml
+import os
+
+_config_path = ''.join([os.path.dirname(__file__), '/../config/config.yaml'])
 
 # if config file not exist, it will raise exception
-# FIXME
-with open('sharehp/config/config.yaml', 'r') as f:
+with open(_config_path, 'r') as f:
     _config = yaml.load(f)
 
 if not _config:
@@ -11,5 +13,7 @@ if not _config:
 
 def get_config(key, default=None):
     return _config.get(key, default)
+
+
 
 
