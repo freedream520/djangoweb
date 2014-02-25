9cf2addca6591726bbb18b0e02383227ad48d189
diaocow <diaocow@163.com>
2014-02-25 20:42:40 +0800

    bugfix: reget token after 50min

--- sharehp/utils/qiniu_helper.py
+++ sharehp/utils/qiniu_helper.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
 import qiniu.rs
 import qiniu.conf
 import qiniu.io
 import config
 import logging
+from datetime import datetime
 from ..exception import ConfigArgumentError
 from ..exception import QiniuUploadFileError
 
@@ -18,10 +20,18 @@ qiniu.conf.SECRET_KEY = _SECRET_KEY
 
 policy = qiniu.rs.PutPolicy(_STATIC_NAME)
 uptoken = policy.token()
+# 上一次获取token时间
+_last_get_token_time = datetime.now()
 
 logger = logging.getLogger('sharehp')
 
 def upload(filepath, key):
+    global  uptoken
+
+    # 每隔50分钟重新获取一次token
+    if int((datetime.now() - _last_get_token_time).total_seconds()) > 50*60:
+        uptoken = policy.token()
+
     try:
         ret, err = qiniu.io.put_file(uptoken, key, filepath)
     except Exception, error: # catch all exception
