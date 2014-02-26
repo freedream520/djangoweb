905a25c5266f0f3fe770f74c817e1d199c027a30
diaocow <diaocow@163.com>
2014-02-26 21:30:38 +0800

    catch exception

--- sharehp/deploy/spide_script/renren_catch.py
+++ sharehp/deploy/spide_script/renren_catch.py
@@ -42,57 +42,60 @@ def main(pages):
         url = _GRAP_URL + '&page=' + str(i)
         html_doc = urllib.urlopen(url).read()
         if html_doc:
-            soup = BeautifulSoup(html_doc)
-            # ============================================================
-            # 采集视频
-            # ============================================================
-            target_divs = soup.find_all('article', {'class': 'post-video'})
-            if target_divs and len(target_divs):
-                for td in target_divs:
-                    # 资源标题
-                    title = td.find('h2')['title']
-                    # 获取视频信息
-                    video_src = td.find('embed')['src'].strip()
-                    video_id = _get_video_id('http://player.56.com/renrenshare_(.*).swf/1030_.*.swf', video_src)
-                    video_img = json.loads(_get_video_info(video_id))['info']['bimg']
-                    # 不抓取重复资源
-                    if db.exists(video_src):
-                        continue
-
-                    resource_info = _deal_resource(video_img)
-                    resource_info['url'] = video_src
-                    resource = db.SpideResource(title, 'video', json.dumps(resource_info), video_src)
-                    db.save(resource)
-                    print '成功爬取视频资源!'
-                    _VIDEO_CNT = _VIDEO_CNT + 1
-
-            # ============================================================
-            # 采集图片
-            # ============================================================
-            target_divs = soup.find_all('article', {'class': ['post-photo', 'post-article']})
-            if target_divs and len(target_divs):
-                for td in target_divs:
-                    post_content = td.find('div', {'class': 'post-content'})
-                    # 资源标题
-                    title = post_content.find('h2')['title']
-                    images = post_content.find_all('img')
-                    # 抓取只有一副图片的资源
-                    if images and len(images) == 1:
-                        image = images[0]
-                        if image.get('data-src', None):
-                            image_src = image['data-src'].strip()
-                        else:
-                            image_src = image['src'].strip()
+            try:
+                soup = BeautifulSoup(html_doc)
+                # ============================================================
+                # 采集视频
+                # ============================================================
+                target_divs = soup.find_all('article', {'class': 'post-video'})
+                if target_divs and len(target_divs):
+                    for td in target_divs:
+                        # 资源标题
+                        title = td.find('h2')['title']
+                        # 获取视频信息
+                        video_src = td.find('embed')['src'].strip()
+                        video_id = _get_video_id('http://player.56.com/renrenshare_(.*).swf/1030_.*.swf', video_src)
+                        video_img = json.loads(_get_video_info(video_id))['info']['bimg']
                         # 不抓取重复资源
-                        if db.exists(image_src):
+                        if db.exists(video_src):
                             continue
 
-                        resource_info = _deal_resource(image_src)
-                        resource = db.SpideResource(title, 'image', json.dumps(resource_info), image_src)
+                        resource_info = _deal_resource(video_img)
+                        resource_info['url'] = video_src
+                        resource = db.SpideResource(title, 'video', json.dumps(resource_info), video_src)
                         db.save(resource)
-                        print '成功爬取图片资源!'
-                        _IMAGE_CNT = _IMAGE_CNT + 1
+                        print '成功爬取视频资源!'
+                        _VIDEO_CNT = _VIDEO_CNT + 1
 
+                # ============================================================
+                # 采集图片
+                # ============================================================
+                target_divs = soup.find_all('article', {'class': ['post-photo', 'post-article']})
+                if target_divs and len(target_divs):
+                    for td in target_divs:
+                        post_content = td.find('div', {'class': 'post-content'})
+                        # 资源标题
+                        title = post_content.find('h2')['title']
+                        images = post_content.find_all('img')
+                        # 抓取只有一副图片的资源
+                        if images and len(images) == 1:
+                            image = images[0]
+                            if image.get('data-src', None):
+                                image_src = image['data-src'].strip()
+                            else:
+                                image_src = image['src'].strip()
+                            # 不抓取重复资源
+                            if db.exists(image_src):
+                                continue
+
+                            resource_info = _deal_resource(image_src)
+                            resource = db.SpideResource(title, 'image', json.dumps(resource_info), image_src)
+                            db.save(resource)
+                            print '成功爬取图片资源!'
+                            _IMAGE_CNT = _IMAGE_CNT + 1
+
+            except Exception, err:
+                print err
         else:
             print '[Error] No data for dealing ...'
     else:
